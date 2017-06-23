b825a1ff6f7857adcc02f7302210e154b9b210c9
Phil Cable <pcable@wsu.edu>
2017-06-23 09:27:11 -0700

    Add back navigation pieces for subsites
    The new design is going to be applied to all sites that use the
    Admission theme - not terribly surprising. So, this beings adding
    back peices that will be needed.
    
    I've opted to not include the `offsite-navigation.php` template as
    the sites don't currently use any offsite menus.

--- functions.php
+++ functions.php
@@ -13,6 +13,7 @@ class WSU_Admission_Theme {
 		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
 		add_filter( 'theme_page_templates', array( $this, 'prune_page_templates' ) );
 		add_filter( 'wp_kses_allowed_html', array( $this, 'allow_download_attribute' ), 10 );
+		add_filter( 'bu_navigation_filter_item_attrs', array( $this, 'bu_navigation_filter_item_atts' ), 10, 2 );
 		add_action( 'wp_footer', array( $this, 'carnegie_tracking_tags' ), 101 );
 		add_action( 'wp_footer', array( $this, 'chegg_conversion_pixels' ), 102 );
 	}
@@ -32,6 +33,40 @@ class WSU_Admission_Theme {
 		unset( $templates['templates/side-right.php'] );
 		unset( $templates['templates/single.php'] );
 		return $templates;
+	}
+
+	/**
+	 * Filter BU Navigation to add the `current` tag to a nav item that matches the requested
+	 * URL when loading one of the admission sub-sites.
+	 *
+	 * @param array  $item_classes
+	 * @param object $page
+	 *
+	 * @return array
+	 */
+	public function bu_navigation_filter_item_atts( $item_classes, $page ) {
+		$page_url = wp_parse_url( $page->url );
+		$page_path = '/';
+
+		if ( ! empty( $page_url ) ) {
+			$page_paths = explode( '/', $page_url['path'] );
+			if ( ! empty( $page_paths[1] ) ) {
+				$page_path = '/' . $page_paths[1] . '/';
+			}
+		}
+
+		$request_paths = explode( '/', $_SERVER['REQUEST_URI'] );
+		$request_path = false;
+
+		if ( ! empty( $request_paths[1] ) ) {
+			$request_path = '/' . $request_paths[1] . '/';
+		}
+
+		if ( in_array( $page_path, array( '/for-counselors/', '/for-parents/', '/for-advisors/' ), true ) && $request_path === $page_path ) {
+			$item_classes[] = 'current';
+		}
+
+		return $item_classes;
 	}
 
 	/**
@@ -124,6 +159,57 @@ class WSU_Admission_Theme {
 			<script type="text/javascript">var ordnumber = Math.random() * 10000000000000;var sscUrl = ("https:" == document.location.protocol ? "https://" : "http://") + "trkn.us/pixel/c?ppt=719&g=requirementslong&gid=3848&ord="+ordnumber+"&v=115";var x = document.createElement("IMG");x.setAttribute("src", sscUrl);x.setAttribute("width", "1");x.setAttribute("height", "1");document.body.appendChild(x);</script>
 			<?php
 		}
+	}
+
+	/**
+	 * Retrieve the ID of the main site for admission.wsu.edu.
+	 *
+	 * @return int ID of the site.
+	 */
+	public static function get_main_site_id() {
+		// The primary domain can be filtered for local development.
+		$home_domain = apply_filters( 'admission_home_domain', 'admission.wsu.edu' );
+
+		$site = get_blog_details( array(
+			'domain' => $home_domain,
+			'path' => '/',
+		) );
+
+		if ( $site ) {
+			return $site->blog_id;
+		}
+
+		return get_current_blog_id();
+	}
+
+	/**
+	 * Determine whether to show the shared, primary navigation from admission.wsu.edu.
+	 *
+	 * @return bool
+	 */
+	public static function show_main_navigation() {
+		$site = get_blog_details();
+
+		// The primary domain can be filtered for local development.
+		$home_domain = apply_filters( 'admission_home_domain', 'admission.wsu.edu' );
+
+		// Site paths that should show the primary navigation. This will include
+		// page paths under these sites.
+		$shared_nav_paths = array(
+			'/',
+			'/for-counselors/',
+			'/for-parents/',
+			'/for-advisors/',
+		);
+
+		// The shared nav paths can be filtered for local development.
+		$shared_nav_paths = apply_filters( 'admission_shared_nav_paths', $shared_nav_paths );
+
+		if ( $home_domain === $site->domain && in_array( $site->path, $shared_nav_paths, true ) ) {
+			return true;
+		}
+
+		return false;
 	}
 }
 
