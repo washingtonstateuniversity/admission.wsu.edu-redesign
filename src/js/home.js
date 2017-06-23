a14b2b3ebc040a8aeb946888933b8d950acef094
Phil Cable <pcable@wsu.edu>
2017-06-23 16:47:40 -0700

    Introduce ripple effect on "Important Announcement" button clicks

--- src/js/home.js
+++ src/js/home.js
@@ -5,4 +5,33 @@
 		$( ".dropped" ).not( this ).removeClass( "dropped" );
 		$( this ).toggleClass( "dropped" );
 	} );
+
+	// Trigger ripple effect on "Important Announcements" button clicks.
+	// Based on https://codepen.io/ahse/pen/gaLKeM.
+	$( ".ripple" ).on( "click", function( e ) {
+		var $ripple = $( "<span class='ripple-effect'></span>" ),
+			$button = $( this ),
+			btnOffset = $button.offset(),
+			xPos = e.pageX - btnOffset.left,
+			yPos = e.pageY - btnOffset.top,
+			size = parseInt( Math.min( $button.height(), $button.width() ) * 0.5 ),
+			animateSize = parseInt( Math.max( $button.width(), $button.height() ) * Math.PI );
+
+		$ripple
+			.css( {
+				top: yPos,
+				left: xPos,
+				width: size,
+				height: size
+			} )
+			.appendTo( $button )
+			.animate( {
+					width: animateSize,
+					height: animateSize,
+					opacity: 0
+				}, 150, function() {
+					$( this ).remove();
+				}
+			);
+	} );
 }( jQuery ) );
