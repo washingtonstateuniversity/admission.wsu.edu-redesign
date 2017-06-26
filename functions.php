98db2dcda606b6e12f75b3db4d048fb2a0efe680
Phil Cable <pcable@wsu.edu>
2017-06-26 15:58:55 -0700

    Bump version 0.0.2

--- functions.php
+++ functions.php
@@ -10,7 +10,7 @@ class WSU_Admission_Theme {
 	/**
 	 * @var string The version of the WSU Admission theme for cache breaking.
 	 */
-	public $version = '0.0.1';
+	public $version = '0.0.2';
 
 	public function __construct() {
 		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
