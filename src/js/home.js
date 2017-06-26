c5f269632021e67fde042c626e7c0bb7e151d7ea
Phil Cable <pcable@wsu.edu>
2017-06-26 11:11:20 -0700

    Introduce home page scroll-based effects

--- src/js/home.js
+++ src/js/home.js
@@ -1,12 +1,12 @@
-( function( $ ) {
+( function( $, window ) {
 
-	// Toggle the "Fields of Study" list.
+	// Toggles the "Fields of Study" list.
 	$( ".drop" ).click( function() {
 		$( ".dropped" ).not( this ).removeClass( "dropped" );
 		$( this ).toggleClass( "dropped" );
 	} );
 
-	// Trigger ripple effect on "Important Announcements" button clicks.
+	// Triggers ripple effect on "Important Announcements" button clicks.
 	// Based on https://codepen.io/ahse/pen/gaLKeM.
 	$( ".ripple" ).on( "click", function( e ) {
 		var $ripple = $( "<span class='ripple-effect'></span>" ),
@@ -34,4 +34,42 @@
 				}
 			);
 	} );
-}( jQuery ) );
+
+	// Triggers scroll-based animations.
+	$( window ).scroll( function() {
+		window.requestAnimationFrame( function() {
+			animate_lazy_text();
+			slide_buttons();
+		} );
+	} );
+
+	// Animates the lazy text list items.
+	function animate_lazy_text() {
+		var container = $( ".lazy-text-scroll" )[ 0 ].getBoundingClientRect();
+
+		if ( window.innerHeight > container.top && 0 < container.bottom ) {
+			$( ".lazy-text-scroll li" ).each( function() {
+				var $element = $( this ),
+					index = $element.index() + 1, // One-based
+					y = $( window ).scrollTop() / ( index * 5 );
+
+				$element.css( "top", "-" + parseInt( y ) + "px" );
+			} );
+		}
+	}
+
+	// Slides the "Important Announcements" buttons in.
+	function slide_buttons() {
+		var container = $( ".announcements" )[ 0 ].getBoundingClientRect();
+
+		if ( window.innerHeight > container.bottom ) {
+			$( ".announcements .slide-up" ).each( function( i ) {
+				var $element = $( this );
+
+				setTimeout( function() {
+					$element.addClass( "animate" );
+				}, i * 250 );
+			} );
+		}
+	}
+}( jQuery, window ) );
