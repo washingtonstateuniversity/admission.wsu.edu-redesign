eb52e2acd70165ad14fc418e00e81d4cce3936e7
Phil Cable <pcable@wsu.edu>
2017-06-28 09:43:28 -0700

    Remove the "Important Announcement" button ripple effect
    This is going to be implemented more widely

--- src/js/home.js
+++ src/js/home.js
@@ -9,35 +9,6 @@
 		$( this ).toggleClass( "dropped" );
 	} );
 
-	// Triggers ripple effect on "Important Announcements" button clicks.
-	// Based on https://codepen.io/ahse/pen/gaLKeM.
-	$( ".ripple" ).on( "click", function( e ) {
-		var $ripple = $( "<span class='ripple-effect'></span>" ),
-			$button = $( this ),
-			btnOffset = $button.offset(),
-			xPos = e.pageX - btnOffset.left,
-			yPos = e.pageY - btnOffset.top,
-			size = parseInt( Math.min( $button.height(), $button.width() ) * 0.5 ),
-			animateSize = parseInt( Math.max( $button.width(), $button.height() ) * Math.PI );
-
-		$ripple
-			.css( {
-				top: yPos,
-				left: xPos,
-				width: size,
-				height: size
-			} )
-			.appendTo( $button )
-			.animate( {
-					width: animateSize,
-					height: animateSize,
-					opacity: 0
-				}, 150, function() {
-					$( this ).remove();
-				}
-			);
-	} );
-
 	// Triggers scroll-based animations.
 	$( window ).scroll( function() {
 		window.requestAnimationFrame( function() {
