8f617fa78f55eafcbba7d5293451b64cfb14950e
Phil Cable <pcable@wsu.edu>
2018-01-30 09:48:35 -0800

    Enqueue a stylesheet for the Campus Tours page

--- functions.php
+++ functions.php
@@ -60,6 +60,10 @@ class WSU_Admission_Theme {
 		if ( is_front_page() ) {
 			wp_enqueue_script( 'home', get_stylesheet_directory_uri() . '/js/home.min.js', array( 'jquery' ), $this->version, true );
 		}
+
+		if ( is_singular( 'json_web_template' ) && is_single( 'campus-tours' ) ) {
+			wp_enqueue_style( 'campus-tours', 'https://admission.wsu.edu/visits/individual/css/individual.css', array( 'spine-theme', 'spine-theme-child' ), $this->version );
+		}
 	}
 
 	public function prune_page_templates( $templates ) {
